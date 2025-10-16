09ea726bd4ee90347edf29384e3286a55828f9df
Helio Frota <00hf11@gmail.com>
2025-10-16 09:05:55 -0300

    test: adds a test for displayName property (#33)

--- nodes/DependencyAnalytics/DependencyAnalytics.spec.ts
+++ nodes/DependencyAnalytics/DependencyAnalytics.spec.ts
@@ -9,3 +9,24 @@ test('It contains the icon', () => {
 	const node = new DependencyAnalytics();
 	expect(node.description.icon).toEqual('file:DependencyAnalytics.svg');
 });
+
+test('It contains all expected displayNames in properties', () => {
+	const node = new DependencyAnalytics();
+	const displayNames = node.description.properties.map((prop: any) => prop.displayName);
+
+	const expectedDisplayNames = [
+		'Authentication Method',
+		'Base URL',
+		'Resource',
+		'Operation',
+		'Operation',
+		'Operation',
+		'Identifier',
+		'Limit',
+		'Input Type',
+		'PURLs',
+		'SBOM SHA-256',
+	];
+
+	expect(displayNames).toEqual(expectedDisplayNames);
+});
