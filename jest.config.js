baa7380292db95226260b6bc19005e91e01c4e3f
Helio Frota <00hf11@gmail.com>
2025-10-19 05:53:19 -0300

    chore: codecov (#36)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
 	preset: 'ts-jest',
 	testPathIgnorePatterns: ['node_modules/', 'dist/'],
+	verbose: true,
+	collectCoverage: true,
+	coverageReporters: ['text', 'cobertura'],
 };
